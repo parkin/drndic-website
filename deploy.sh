911760b598da254a4ec676e8695ed6537d72e04f
Will Parkin <wmparkin@gmail.com>
2014-07-22 20:30:23 -0400

    Website at Tue Jul 22 20:30:23 EDT 2014

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -ex
+
+REPO="root@drndiclab-bkup.physics.upenn.edu:/c/shared/Website/drndicgroup.git"
+
+PUBLISH_BRANCH=publish
+DIR=temp-publish-clone
+WEBSITE_DIR=_site
+
+# update the sphinx documentation
+grunt build
+jekyll build
+
+# Delete any existing temporary website clone
+rm -rf $DIR
+
+# Clone the current repo into temp folder
+# done this way so we only check out the publish branch
+mkdir $DIR
+cd $DIR
+git init
+git remote add -t $PUBLISH_BRANCH -f origin $REPO
+git checkout $PUBLISH_BRANCH
+
+# Delete everything
+rm -rf *
+
+# Copy website files from real repo
+# Use rsync so we can ignore hidden files
+rsync -av --exclude=".*" ../$WEBSITE_DIR/* .
+
+# Stage all files in git and create a commit
+git add .
+git add -u
+git commit -m "Website at $(date)"
+
+# Push the new files up to GitHub
+git push origin $PUBLISH_BRANCH
+
+# Delete our temp folder
+cd ..
+rm -rf $DIR
