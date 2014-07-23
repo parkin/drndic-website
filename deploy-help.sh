0d20162c0d3c1ecdc8cf9986a591b74f444fa16d
Will Parkin <wmparkin@gmail.com>
2014-07-22 22:34:22 -0400

    fixed links

--- deploy-help.sh
+++ deploy-help.sh
@@ -2,29 +2,23 @@
 
 set -ex
 
-REPO="root@drndiclab-bkup.physics.upenn.edu:/c/shared/Website/drndicgroup.git"
-
 PUBLISH_BRANCH=$1
-DIR=temp-publish-clone
 WEBSITE_DIR=_site
 
-# Delete any existing temporary website clone
-rm -rf $DIR
+# get the current branch name
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+branch_name="(unnamed branch)"     # detached HEAD
 
-# Clone the current repo into temp folder
-# done this way so we only check out the publish branch
-mkdir $DIR
-cd $DIR
-git init
-git remote add -t $PUBLISH_BRANCH -f origin $REPO
+branch_name=${branch_name##refs/heads/}
+
+# Move to the publish branch
+
 git checkout $PUBLISH_BRANCH
-
-# Delete everything
-rm -rf *
+git pull
 
 # Copy website files from real repo
 # Use rsync so we can ignore hidden files
-rsync -av --exclude=".*" ../$WEBSITE_DIR/* .
+rsync -av --exclude=".*" $WEBSITE_DIR/* .
 
 # Stage all files in git and create a commit
 git add .
@@ -34,6 +28,5 @@ git commit -m "Website at $(date)"
 # Push the new files up to GitHub
 git push origin $PUBLISH_BRANCH
 
-# Delete our temp folder
-cd ..
-rm -rf $DIR
+# move back to the old branch
+git checkout $branch_name
