7436a4bb967a28a442d8edf98f0482c55451babb
Will Parkin <wmparkin@gmail.com>
2014-07-24 11:57:00 -0400

    fixed bugs

--- serve.sh
+++ serve.sh
@@ -2,18 +2,28 @@
 #
 # This script will run grunt and jekyll serve simultaneously
 
+# $1 - exit code
 usage () {
+  local exit_code=$1
+  if ! [[ $exit_code =~ '^[0-9]+$' ]] ; then
+    exit_code=1
+  fi
   echo -e "$0 starts grunt and the jekyll server with watch"
   echo -e "Usage: $0 [-b <string:baseurl>]"
   echo -e "\t-b: baseurl for jekyll config"
+
+  exit $exit_code;
 }
 
 b=""
 
-while getopts "b:" o; do
+while getopts "b:h" o; do
   case "${o}" in
     b)
       b=${OPTARG}
+      ;;
+    h)
+      usage 0
       ;;
     *)
       usage
